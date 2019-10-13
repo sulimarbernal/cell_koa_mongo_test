e0c956e186ac206b8ff0e6c9a126d28c4fd33800
Ernesto Fuentes <ernesto.fuentes@peanuthub.cl>
2019-10-12 22:47:50 -0300

    test(config): added more tests to config module

--- src/utils/config/index.test.ts
+++ src/utils/config/index.test.ts
@@ -4,6 +4,14 @@ process.env.LOG_LEVEL = 'debug'
 import config from './index'
 
 describe('Configuration client test', (): void => {
+  it('should get empty string if no key is found', (): void => {
+    expect(config.get('NO_KEY')).toEqual('')
+  })
+
+  it('should get a valid value if key is found', (): void => {
+    expect(config.get('NODE_ENV')).toEqual('TEST')
+  })
+
   it('should get all environment variables', (): void => {
     const vars = config.getAll()
 
